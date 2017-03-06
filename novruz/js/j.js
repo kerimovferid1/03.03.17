6ade1659de57f3002156f97f50e86a63a2f36462
kerimovferid1 <kerimovferid98@gmail.com>
2017-03-06 09:15:01 +0400

    Update j.js

--- novruz/js/j.js
+++ novruz/js/j.js
@@ -1,13 +1,14 @@
 var bayram = new Date("Mar 20, 2017 00:00:00").getTime();
-var indi = new Date().getTime();
 
-var x= setInterval(function(){
+function k(){
+	var indi = new Date().getTime();
+
 	    var qalib = bayram - indi;
 
 	  var gun= Math.floor(qalib / 86400000); // days
-var saat = Math.floor((qalib % 86400000) / 3600000); // hours
-var deq = Math.round(((qalib % 86400000) % 3600000) / 60000);
+	var saat = Math.floor((qalib % 86400000) / 3600000); // hours
+	var deq = Math.round(((qalib % 86400000) % 3600000) / 60000);
     var saniye= Math.floor((qalib%(60000))/(1000));
 
 document.getElementById("timer").innerHTML = ("Bayrama" +gun+ " gun " + saat + " saat " + deq + " deqiqe" + saniye+ "saniye qalib");
-},1000)
+} setInterval(k , 1000);
